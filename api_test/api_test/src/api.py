8f469b37f499ada26d4247a8c8b1eebc414a97ee
ebenezer <ebensemere@icloud.com>
2022-11-12 22:49:50 -0800

    file init

---
+++ api_test/api_test/src/api.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from src.endpoints import ingest, test
+from src.parametric.find_location import Find
+from src.util.settings import *
+import faulthandler 
+import json
+
+# initialize fast api
+app = FastAPI(title= "Parametric Piecewise")
+faulthandler.enable()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+@app.get("/")
+def home():
+        resources = (
+            "Title": "Ebenezer's Application"
+        )
+        return resources
